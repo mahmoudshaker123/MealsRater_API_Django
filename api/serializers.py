82e053cbfd3aaeea564fad048df12a3fc365abba
mahmoud <mahmoud.shaker123123@gmail.com>
2023-02-21 17:33:03 +0200

    "second commit"

---
+++ api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import *
+
+
+class MealSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Meal
+        fields = ('id','title','description')
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ('id','meal','user','stars')
+        
